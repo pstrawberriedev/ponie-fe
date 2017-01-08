aa126a227b66995cd4cad485a7876239cceefe69
JD <pstrawberrie@gmail.com>
2017-01-07 18:50:59 -0700

    started styling!

--- gulpfile.js
+++ gulpfile.js
@@ -10,6 +10,7 @@ var plumber = require('gulp-plumber');
 var concat = require('gulp-concat');
 var rename = require('gulp-rename');
 var runSequence = require('run-sequence');
+var liveReload = require('gulp-livereload');
 
 // Clean
 gulp.task('clean', function(){
@@ -37,34 +38,49 @@ gulp.task('hbs', function () {
     }
 
     return gulp.src('./src/views/index.hbs')
+      .pipe(plumber(function (error) {
+        console.log(error.message);
+        this.emit('end');
+      }))
       .pipe(handlebars(templateData, options))
       .pipe(rename('index.html'))
       .pipe(gulp.dest('dist'))
+      .pipe(liveReload())
 });
 
 //Babel
 gulp.task('babel', function() {
     return gulp.src('./src/js/*.js')
+      .pipe(plumber(function (error) {
+        console.log(error.message);
+        this.emit('end');
+      }))
       .pipe(babel({ presets: ['es2015'] }))
       .pipe(concat('app.js'))
       .pipe(uglify())
       .pipe(rename({suffix: '.min'}))
       .pipe(gulp.dest('./dist/js'))
+      .pipe(liveReload())
 });
 
 // Less
 gulp.task('less', function() {
     return gulp.src('./src/less/styles.less')
-      .pipe(plumber())
+       .pipe(plumber(function (error) {
+         console.log(error.message);
+         this.emit('end');
+       }))
       .pipe(less({ paths: ['./src/less'] }))
       .pipe(cssmin())
       .pipe(rename({suffix: '.min'}))
       .pipe(gulp.dest('./dist/css'))
+      .pipe(liveReload())
 });
 
 // +++
 // Watch
 gulp.task('watch', function() {
+    //liveReload.listen();
     gulp.watch(['./src/**/*.hbs'], ['hbs']);
     gulp.watch(['./src/**/*.less'], ['less']);
     gulp.watch(['./src/**/*.js'], ['babel']);
